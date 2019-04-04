28a4e8d213443982c9c2f6caf17f8c04d35f85ba
Jakub Konka <jakub.konka@golem.network>
2019-04-04 16:27:46 +0200

    Add simple pwd functionality

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,35 @@
+use nix::unistd;
 use std::io::{self, Write};
 
 #[derive(PartialEq)]
 enum Status {
-    EXIT,
+    Ok,
+    Exit,
+}
+
+fn pwd() -> Status {
+    let dir = unistd::getcwd().expect("cannot get present working directory");
+    println!("{}", dir.to_str().expect("failed to convert to str"));
+    io::stdout().flush().expect("failed to flush stdout");
+
+    Status::Ok
 }
 
 fn lsh_exec<'a, It>(args: It) -> Status
 where
     It: IntoIterator<Item = &'a str>,
 {
-    Status::EXIT
+    let args: Vec<&str> = args.into_iter().collect();
+    if args.is_empty() {
+        return Status::Ok;
+    }
+
+    let cmd = args[0];
+    match cmd {
+        "pwd" => pwd(),
+        "exit" => Status::Exit,
+        _ => Status::Ok,
+    }
 }
 
 fn lsh_loop() {
@@ -22,7 +42,7 @@ fn lsh_loop() {
             .read_line(&mut line)
             .unwrap_or_else(|err| panic!("unexpected error occurred reading from stdin: {}", err));
 
-        if lsh_exec(line.split_whitespace()) == Status::EXIT {
+        if lsh_exec(line.split_whitespace()) == Status::Exit {
             break;
         }
     }
